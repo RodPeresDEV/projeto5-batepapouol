9b36acd60c33170fd2a9f235a2fc950010cdd6d0
Rodrigo Peres <rperes.batista@gmail.com>
2023-01-23 03:17:07 -0300

    versão com chat funcional

--- script/script.js
+++ script/script.js
@@ -1,59 +1,87 @@
-//let nome = prompt("Qual seu nome?");
 let mensagensChat = [];
+let nome = "";
+const chat = document.querySelector(".chat");
 
+function logarUsuario() {
+  nome = prompt("Qual seu nome?");
+  let logar = axios.post(
+    "https://mock-api.driven.com.br/api/v6/uol/participants",
+    { name: nome }
+  );
 
-const chat = document.querySelector('.chat');
+  logar.then((res) => {
+    console.log(res);
+    buscarMensagens();
+    setInterval(manterConxão, 5000);
+    setInterval(buscarMensagens, 5000);
+  });
+  logar.catch(() => {
+    logarUsuario();
+  });
+}
 
-console.log(chat)
-buscarMensagens();
+logarUsuario();
+
+function manterConxão() {
+  axios.post("https://mock-api.driven.com.br/api/v6/uol/status", {
+    name: nome,
+  });
+}
+
 //buscando mensagens do servidor
 function buscarMensagens() {
-    let buscaMsg = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
-    buscaMsg.then(mostrarTodasMensagens);
-    buscaMsg.catch();
+  let buscaMsg = axios.get(
+    "https://mock-api.driven.com.br/api/v6/uol/messages"
+  );
+  buscaMsg.then(mostrarTodasMensagens);
+  buscaMsg.catch();
 }
 
 function mostrarTodasMensagens(resposta) {
-    console.log(resposta.data);
+  chat.innerHTML = "";
+  const todasMensagens = resposta.data;
+  for (let i = 0; i < todasMensagens.length; i++) {
+    const time = todasMensagens[i].time;
+    const from = todasMensagens[i].from;
+    const text = todasMensagens[i].text;
+    const to = todasMensagens[i].to;
+    const type = todasMensagens[i].type;
 
-    const todasMensagens = resposta.data;
-    for (let i = 0; i < todasMensagens.length; i++) {
-        const time = todasMensagens[i].time;
-        const from = todasMensagens[i].from;
-        const text = todasMensagens[i].text;
-        const to = todasMensagens[i].to;
-        const type = todasMensagens[i].type;
+    if (type === "private-message" && (from === nome || to === nome)) {
+      chat.innerHTML += `
+                <div class="todas-msgs private" data-test="message">
+                    <p> <span class="tempo-chat">(${time})</span> <span class="name-chat">${from}</span>${text}</p>
+                </div>`;
+    } else if (type === "status") {
+      chat.innerHTML += `
+                <div class="todas-msgs status" data-test="message">
+                    <p> <span class="tempo-chat">(${time})</span> <span class="name-chat">${from}</span>${text}</p>
+                </div>`;
+    } else {
+      chat.innerHTML += `
+                <div class="todas-msgs" data-test="message">
+                    <p> <span class="tempo-chat">(${time})</span> <span class="name-chat">${from}</span> para <span class="name-chat">${to}:</span>${text}</p>
+                </div>`;
+    }
+  }
 
-        if ( type === 'private-message') {
-            
-            chat.innerHTML += `<li class="todas-msgs private" data-test="message"><span class="tempo-chat">(${time})</span> <span class="name-chat">${from}</span> ${text}</li>`;
-        } else if ( type === 'status' ) {
-            chat.innerHTML += `<li class="todas-msgs status" data-test="message"><span class="tempo-chat">(${time})</span> <span class="name-chat">${from}</span> ${text}</li>`;
-        } else {
-            chat.innerHTML += `<li class="todas-msgs" data-test="message"><span class="tempo-chat">(${time})</span> <span class="name-chat">${from}</span> para <span class="name-chat">${to}:</span> ${text}</li>`;
-        }
-        
-        console.log(todasMensagens[i].text);
-    } 
-
+  document.querySelector(".todas-msgs:last-child").scrollIntoView();
 }
 
-function naoMostrouMensagens(res) {
-    console.log('Mensagens não carregadas')
+function enviarMensagens() {
+  let mensagemDigitada = document.querySelector("input");
+  let enviandoMensagem = {
+    from: nome,
+    to: "Todos",
+    text: mensagemDigitada.value,
+    type: "message",
+  };
+  axios
+    .post("https://mock-api.driven.com.br/api/v6/uol/messages", enviandoMensagem)
+    .then(()=>{
+        mensagemDigitada.value = '';
+        buscarMensagens()})
+    .catch(() => {
+      window.location.reload();
+    });
 }
-
-
-
-function msgEnviada(res) {
-//    console.log("A mensagem foi recebida pelo servidor");
-
-}
-
-//verificação usuário online
-//function verificandoOnline() {
-//    let ping = axios.post('https://mock-api.driven.com.br/api/v6/uol/status', nomeUsuario);
-//    ping.then(estaOn);
-//    ping.catch(estaOff);
-//}
-
-//setInterval(verificandoOnline, 5000)
